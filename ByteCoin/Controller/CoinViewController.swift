8b207a714c51e3bf06c8ef38786d0510c15aeb2e
karayilanberat <karayilanberat@gmail.com>
2024-07-18 16:07:17 +0300

    End Of The Project

---
+++ ByteCoin/Controller/CoinViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CoinViewController: UIViewController {
+    
+    let coinManager = CoinManager()
+
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
+    }
+
+
+}
+
+//MARK: - UIPickerViewDelegate, UIPickerViewDataSource
+
+extension CoinViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let selectedCoin = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCoin)
+    }
+}
+
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,9 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by berat on 18.07.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
